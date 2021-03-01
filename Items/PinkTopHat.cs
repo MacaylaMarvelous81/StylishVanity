59d8e5e4d3715aceef09f68248fbd710537410cf
MacaylaMarvelous81 <admin@thelegendaryorb.tk>
2021-02-28 20:36:33 -0800

    Add Pink Top Hat Changes: - Add Pink Top Hat (needs tweaking) - Rename the Pink Balloon

--- Items/PinkTopHat.cs
+++ Items/PinkTopHat.cs
@@ -4,34 +4,34 @@ using Terraria.ModLoader;
 
 namespace StylishVanity.Items
 {
-	public class PinkBalloon : ModItem
+	[AutoloadEquip(EquipType.Head)]
+	public class PinkTopHat : ModItem
 	{
 		public override void SetStaticDefaults()
 		{
-			DisplayName.SetDefault("TirzahTheWhale's Pink Balloon");
-			Tooltip.SetDefault("A balloon dedicated to TirzahTheWhale.");
+			DisplayName.SetDefault("TirzahTheWhale's Pink Top Hat");
+			Tooltip.SetDefault("A top hat dedicated to TirzahTheWhale.");
 		}
 		public override void SetDefaults()
 		{
-			item.width = 18;
-			item.height = 32;
-			item.value = 17000; // 1 Gold 70 Silver
+			item.width = 20;
+			item.height = 14;
+			item.value = 4000; // 40 Silver
 			item.rare = 1; // Blue
 			item.vanity = true;
-			item.holdStyle = 1; // When selected hold like torches
 		}
 		public override bool CanUseItem(Player player)
 		{
 			return false;
 		}
-		public override void HoldItem(Player player)
-		{
-			player.itemLocation.X = player.Center.X - 2 * player.direction;
-		}
+		/*
+		public override void UpdateVanity(Player player, EquipType type)
+		{}
+		*/
 		public override void AddRecipes()
 		{
 			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ItemID.ShinyRedBalloon);
+			recipe.AddIngredient(ItemID.TopHat);
 			recipe.AddIngredient(ItemID.PinkPricklyPear);
 			recipe.AddTile(TileID.TinkerersWorkbench);
 			recipe.SetResult(this);
