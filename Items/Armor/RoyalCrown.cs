8669fdfd814c16e9c96d45f64ae49174c7593e88
Jomar Milan <ilovetirzah@gmail.com>
2022-07-03 21:51:30 -0700

    Fix parts tModPorter did not port in RoyalCrown

--- Items/Armor/RoyalCrown.cs
+++ Items/Armor/RoyalCrown.cs
@@ -9,6 +9,7 @@ namespace StylishVanity.Items.Armor
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Royal Crown");
+			ArmorIDs.Head.Sets.DrawFullHair[Item.headSlot] = true;
 		}
 		public override void SetDefaults()
 		{
@@ -22,43 +23,7 @@ namespace StylishVanity.Items.Armor
 		{
 			return false;
 		}
-		public override void DrawHair(ref bool drawHair, ref bool drawAltHair)/* tModPorter Note: Removed. In SetStaticDefaults, use ArmorIDs.Head.Sets.DrawFullHair[Item.headSlot] = true if you had drawHair set to true, and ArmorIDs.Head.Sets.DrawHatHair[Item.headSlot] = true if you had drawAltHair set to true */
-		{
-			drawHair = true;
-		}
-		/*
-		public override bool IsVanitySet(int head, int body, int legs)
-		{
-			Item headItem = new Item();
-			Item bodyItem = new Item();
-			Item legItem = new Item();
-			if (head >= 0)
-			{
-				headItem.SetDefaults(Item.headType[head], true);
-			}
-			if (body >= 0)
-			{
-				bodyItem.SetDefaults(Item.bodyType[body], true);
-			}
-			if (legs >= 0)
-			{
-				legItem.SetDefaults(Item.legType[legs], true);
-			}
-			return headItem.type == ModContent.ItemType<RoyalCrown>() && bodyItem.type == ModContent.ItemType<RoyalCapeTop>() && legItem.type == ModContent.ItemType<RoyalCapeBottom>();
-		}
-		*/
-		/*
-		public override void UpdateVanitySet(Player player)
-		{
-			// Spawn dust 1/10 of the time
-			if (Main.rand.Next(10) == 0)
-			{
-				// TODO: Update to DustID if tModLoader/tModLoader#1343 gets merged
-				Dust.NewDust(player.position, player.width, player.height, 133);
-			}
-		}
-		*/
-		public override void UpdateVanity(Player player, EquipType type)
+		public override void EquipFrameEffects(Player player, EquipType type)
 		{
 			// Spawn dust 1/10 of the time
 			if (Main.rand.Next(10) == 0) {
